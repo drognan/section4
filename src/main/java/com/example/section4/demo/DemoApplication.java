931c55b114b3df644114a94fad82a8287894045e
Björn Stålfors <bjaxxen@gmail.com>
2021-12-06 19:12:44 +0000

    Egen banner

--- src/main/java/com/example/section4/demo/DemoApplication.java
+++ src/main/java/com/example/section4/demo/DemoApplication.java
@@ -1,7 +1,7 @@
 package com.example.section4.demo;
 
 
-import com.example.section4.demo.services.JokeServiceImpl;
+import com.example.section4.demo.services.JokeService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +12,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		var ctx = SpringApplication.run(DemoApplication.class, args);
 
-		var service = (JokeServiceImpl) ctx.getBean("jokeServiceImpl");
-
-
+		// Cast concrete bean to interface
+		var service = (JokeService) ctx.getBean("jokeServiceImpl");
 		System.out.println(service.makeJoke());
 	}
 
